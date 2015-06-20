0cfe532478428a3d5fd371f61b367f04516407c6
rcchen <chenpala@gmail.com>
2015-06-20 17:12:03 +0800

    finish ch6

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,13 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy] 
+
   def index
     @groups = Group.all
   end
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def new
@@ -12,11 +15,11 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
   end
 
   def create
-     @group = Group.create(group_params)
+     @group = current_user.groups.new(group_params)
 
     if @group.save
       redirect_to groups_path
@@ -26,7 +29,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
       if @group.update(group_params)
       redirect_to groups_path, notice: "修改成功"
     else
@@ -36,7 +39,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
     @group.destroy
     redirect_to groups_path, alert: "刪除成功！！"
   end
